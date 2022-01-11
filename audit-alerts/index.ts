79e5a40dd33f0e695b9676d976192482b2ef7e64
Evan Shortiss <evanshortiss@gmail.com>
2022-01-11 13:24:59 -0800

    fix: error handling for audit alerts service

--- audit-alerts/index.ts
+++ audit-alerts/index.ts
@@ -1,11 +1,15 @@
-import express from 'express'
+import express, { ErrorRequestHandler } from 'express'
 import sendgrid from '@sendgrid/mail'
 import log from 'barelog'
 import { HTTP } from 'cloudevents'
 import { get } from 'env-var'
 import { json } from 'body-parser'
 
+require('dotenv').config()
+
 const SENDGRID_API_KEY = get('SENDGRID_API_KEY').required().asString()
+const EMAIL_FROM = get('EMAIL_FROM').required().asString()
+const EMAIL_TO = get('EMAIL_TO').required().asString()
 const HTTP_PORT = get('HTTP_PORT').default(8080).asPortNumber()
 const app = express()
 
@@ -21,17 +25,28 @@ app.post('/*', json(), async (req, res, next) => {
 
   try {
     const msg = {
-      to: 'eshortis@redhat.com', // Change to your recipient
-      from: 'eshortis@redhat.com', // Change to your verified sender
+      from: EMAIL_FROM,
+      to: EMAIL_TO,
       subject: 'Audit Failure Detected',
       text: `Received an audit failure!`,
     }
 
     await sendgrid.send(msg)
+    
+    log(`moderation email sent from ${EMAIL_FROM} to ${EMAIL_TO}`)
+
+    res.status(202).end()
   } catch (e) {
     next(e)
   }
 })
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  log('error processing request', err)
+
+  res.status(500).end('internal server error')
+}
+app.use(errorHandler)
 
 app.listen(HTTP_PORT, () => {
   log(`audit-alerts listening on ${HTTP_PORT}`)
