96c2aee2fe331995614344cd834eea2fe6dfca69
Evan Shortiss <evanshortiss@gmail.com>
2022-01-25 16:08:51 -0800

    use the shipwars-bonuses topic by default for generated payloads

--- bonus-producer/config.js
+++ bonus-producer/config.js
@@ -6,5 +6,5 @@ module.exports = {
   LOG_LEVEL: get('LOG_LEVEL').default('info').asString(),
   HTTP_PORT: get('HTTP_PORT').default(8080).asPortNumber(),
   KAFKA_CLIENT_ID: get('KAFKA_CLIENT_ID').default('shot-producer').asString(),
-  KAFKA_TOPIC: get('KAFKA_TOPIC').default('shots').asString()
+  KAFKA_TOPIC: get('KAFKA_TOPIC').default('shipwars-bonuses').asString()
 };
